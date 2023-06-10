ff04e4e34885dcc4324a55947c011d2b2794e934
Dinh Du <20520159@gm.uit.edu.vn>
2023-06-10 16:05:21 +0700

    Bottom Tab Bar

---
+++ lib/navigations/tabbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Tabbar extends StatefulWidget {
+  const Tabbar({super.key});
+
+  @override
+  State<Tabbar> createState() => _TabbarState();
+}
+
+class _TabbarState extends State<Tabbar> {
+  int _selectedTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (index) {
+          setState(() {
+            _selectedTab = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_music), label: "For you"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.library_music_outlined), label: "Library"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
+        ],
+      ),
+    );
+  }
+}
